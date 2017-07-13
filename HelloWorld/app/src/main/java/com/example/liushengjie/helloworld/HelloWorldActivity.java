cfdca876491dbb23a7ed487525ae872ffa7328ee
shengjie <liushengchieh@gmail.com>
2017-07-13 10:45:58 +0800

    first commit

---
+++ HelloWorld/app/src/main/java/com/example/liushengjie/helloworld/HelloWorldActivity.java
@@ -0,0 +1,19 @@
+package com.example.liushengjie.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class HelloWorldActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hello_world_layout);
+        //Log.d("HelloWorldActivity", "onCreate execute");
+        //Log.d("HelloWorldActivity", "onCreate: execute");
+        Log.d("data", "onCreate: execute");
+    }
+
+    //private static final String TAG = "HelloWorldActivity";
+}
