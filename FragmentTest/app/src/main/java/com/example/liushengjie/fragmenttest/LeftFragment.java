c9cfbab3c5f76db86ef25bd1454d2f83fdc66e68
shengjie <liushengchieh@gmail.com>
2017-07-21 12:57:19 +0800

    fragmenttest

---
+++ FragmentTest/app/src/main/java/com/example/liushengjie/fragmenttest/LeftFragment.java
@@ -0,0 +1,21 @@
+package com.example.liushengjie.fragmenttest;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by liushengjie on 2017/7/19.
+ */
+
+public class LeftFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.left_fragment, container, false);
+        return view;
+    }
+}
