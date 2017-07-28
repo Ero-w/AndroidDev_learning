1d3b1df5243e60d612dec491d4f56346f028cf65
shengjie <liushengchieh@gmail.com>
2017-07-28 17:43:17 +0800

    chapter6,database

---
+++ LitePalTest/app/src/main/java/com/example/liushengjie/litepaltest/Category.java
@@ -0,0 +1,27 @@
+package com.example.liushengjie.litepaltest;
+
+/**
+ * Created by liushengjie on 2017/7/28.
+ */
+
+public class Category {
+
+    private  int id;
+
+    private String categoryName;
+
+    private int categoryCode;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCategocyName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public void setCategoryCode(int categoryCode) {
+        this.categoryCode = categoryCode;
+    }
+
+}
---
+++ LitePalTest/app/src/main/java/com/example/liushengjie/litepaltest/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.liushengjie.litepaltest;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import org.litepal.LitePal;
+import org.litepal.crud.DataSupport;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button createDatabase = (Button) findViewById(R.id.create_database);
+        createDatabase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LitePal.getDatabase();
+            }
+        });
+
+        Button addData = (Button) findViewById(R.id.add_data);
+        addData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Book book = new Book(); //创建Book的实例
+                //调用Book类中的set方法
+                book.setName("The Da Vinci Code");
+                book.setAuthor("Dan Brown");
+                book.setPages(454);
+                book.setPrice(16.96);
+                book.setPress("Unknow");
+                book.save(); //数据添加(继承自DataSupport类)
+            }
+        });
+
+        Button updateData = (Button) findViewById(R.id.update_data);
+        updateData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Book book = new Book(); //创建Book的实例
+                book.setPrice(14.95);
+                book.setPress("Anchor");
+                book.updateAll("name = ? and author = ?", "The Lost Symbol", "Dan Brown");
+            }
+        });
+
+        Button deleteData = (Button) findViewById(R.id.delete_data);
+        deleteData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DataSupport.deleteAll(Book.class, "price < ?", "15");
+            }
+        });
+
+        Button queryData = (Button) findViewById(R.id.query_data);
+        queryData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                List<Book> books = DataSupport.findAll(Book.class);
+                for (Book book: books) {
+                    Log.d("MainActivity", "book name is " + book.getName());
+                    Log.d("MainActivity", "book author is " + book.getAuthor());
+                    Log.d("MainActivity", "book pages is " + book.getPages());
+                    Log.d("MainActivity", "book price is " + book.getPrice());
+                    Log.d("MainActivity", "book press is " + book.getPress());
+                }
+            }
+        });
+
+
+
+
+    }
+
+}
+
+
